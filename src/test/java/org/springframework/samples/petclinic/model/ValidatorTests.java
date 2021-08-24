9eef8b661924889f88efc184a3a51b380b0a8841
stephenwu <stephenwu@nbnco.com.au>
2021-08-24 15:24:33 +1000

    Revert "modifitied validatorTests with a new provisoned in the createValidator"
    This reverts commit 12311270fc9a8f6dc379185e6c7479a436ecb64c.

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -34,7 +34,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 class ValidatorTests {
 
-	private Validator createValidator("newValidator") {
+	private Validator createValidator() {
 		LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
 		localValidatorFactoryBean.afterPropertiesSet();
 		return localValidatorFactoryBean;
